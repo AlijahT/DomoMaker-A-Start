59705fe98d3c00c9c504a60e741cfee779abf066
Alijah Trombley <59921609+AlijahT@users.noreply.github.com>
2023-04-01 17:01:33 -0400

    attempted to fix problem

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -2,14 +2,13 @@ const requiresLogin = (req, res, next) => {
     if(!req.session.account){
         return res.redirect('/');
     }
-    return next()
+    return next();
 }
 
 const requiresLogout = (req, res, next) => {
     if (req.session.account){
         return res.redirect('/maker');
     }
-
     return next();
 }
 
